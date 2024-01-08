1f16b6a29da87d1ef81c544e8375d0c42f3eebdd
SF97 <a.uchaev97@gmail.com>
2024-01-08 17:28:33 +0500

    Copy text support

--- chat_item.py
+++ chat_item.py
@@ -1,24 +1,30 @@
 import flet as ft
+import pyperclip
 from message import Message
 
-class ChatItem(ft.Row):
+class ChatItem(ft.ListTile):
     def __init__(self, message: Message):
         super().__init__()
-        self.vertical_alignment = "start"
         is_gpt_answer = message.is_gpt_answer 
         title = "FletGPT" if is_gpt_answer else "User"
-        self.controls = [
-            ft.CircleAvatar(
-                content=ft.Icon(ft.icons.ANDROID if is_gpt_answer else ft.icons.PERSON),
-                color=ft.colors.WHITE,
-                bgcolor=ft.colors.INDIGO if is_gpt_answer else ft.colors.GREEN,
-            ),
-            ft.Column(
-                [
-                    ft.Text(title, weight="bold"),
-                    ft.Text(message.text, selectable=True, width=500),
-                ],
-                spacing=1,
-            ),
-            
-        ]
+        self.leading=ft.CircleAvatar(
+                            content=ft.Icon(ft.icons.ANDROID if is_gpt_answer else ft.icons.PERSON),
+                            color=ft.colors.WHITE,
+                            bgcolor=ft.colors.INDIGO if is_gpt_answer else ft.colors.GREEN,
+                        )
+        self.title=ft.Text(title, weight="bold")
+        self.subtitle=ft.Text(message.text, selectable=True)
+        self.content_padding=ft.padding.all(0.0)
+        self.dense=True
+        self.is_three_line=True
+        
+        def copy_text(_):
+            pyperclip.copy(message.text)
+        
+        if (is_gpt_answer):
+            self.trailing=ft.IconButton(
+                            on_click=copy_text,
+                            icon=ft.icons.COPY,
+                            icon_color=ft.colors.GREEN,
+                            icon_size=16,       
+                        )
