525ec43088111d5907aa21863396d92725e34232
SF97 <a.uchaev97@gmail.com>
2024-01-26 22:32:12 +0500

    add gpt models and settings

---
+++ src/app/gpt_model.py
@@ -0,0 +1,26 @@
+class GPTModel:
+    def __init__(self):
+        pass
+
+    GPT_35_TURBO = "gpt-3.5-turbo"
+    GPT_35_TURBO_16K = "gpt-3.5-turbo-16k"
+    PT_35_TURBO_16K_0613 = "gpt-3.5-turbo-16k-0613"
+    GPT_35_LONG = "gpt-3.5-long"
+
+    GPT_4 = "gpt-4"
+    GPT_4_0613 = "gpt-4-0613"
+    GPT_4_32K = "gpt-4-32k"
+    GPT_4_32K_0613 = "gpt-4-32k-0613"
+    GPT_4_TURBO = "gpt-4-turbo"
+
+    all = [
+        GPT_35_TURBO,
+        GPT_35_TURBO_16K,
+        PT_35_TURBO_16K_0613,
+        GPT_35_LONG,
+        GPT_4,
+        GPT_4_0613,
+        GPT_4_32K,
+        GPT_4_32K_0613,
+        GPT_4_TURBO,
+    ]
---
+++ src/app/user_settings.py
@@ -0,0 +1,7 @@
+class UserSettings:
+    def __init__(self):
+        pass
+    
+    CURRENT_CHAT_ID = "current_chat_id"
+    CURRENT_MODEL = "current_model"
+    CONTEXT_DEPTH = "context_depth"
